4c4fac88a135e49f72a3e6d3f44a36273e5f2b25
DANIEL ALEJANDRO RUIZ POLI <danielruiz368@gmail.com>
2025-04-05 07:38:17 +0200

    update styles in mobile view

---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  // Configuración básica
+  root: '.', // Define la raíz del proyecto (donde está tu index.html)
+  publicDir: 'public', // Define el directorio público (donde irán los archivos estáticos como imágenes, fuentes, etc.)
+  build: {
+    outDir: 'dist', // Carpeta donde se generarán los archivos después de la compilación
+    assetsDir: 'assets', // Carpeta dentro de dist para los archivos estáticos como imágenes
+  },
+  server: {
+    port: 3000, // Puerto del servidor de desarrollo
+    open: true, // Abre el navegador automáticamente al iniciar el servidor
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "sass:math";`
+      },
+    },
+  },
+});
